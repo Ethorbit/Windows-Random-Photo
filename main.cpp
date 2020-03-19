33ee08963e161af22e7376c3b5c26b95a0c3a0fa
Ethorbit <hi1212003@gmail.com>
2020-03-19 04:19:37 -0700

    Disallow more than 1 instance from running

--- main.cpp
+++ main.cpp
@@ -82,6 +82,62 @@ private:
 
 int uimain(std::function<int()> run)
 {
+	LPVOID uniqueID = nullptr; // Only used if this is a different instance to identify the original
+
+	// Save first argument as it will be img path due to opening image with program:
+	const wchar_t* openImage = L"";
+	if (sciter::application::argv().size() >= 2)
+		openImage = sciter::application::argv().at(1).c_str();
+	
+	// Generate our unique program identifier:
+	wchar_t mutexName[100];
+	swprintf_s(mutexName, sizeof(mutexName) / sizeof(wchar_t), L"sRandomPhoto%i", _getpid());
+
+	//////////////////////////////////////////////////////////////////////////////////
+	// Save PID to memory, if it's been saved already then this is a new instance.
+	// From here we can stop multiple instances from running:
+	HANDLE openMap = OpenFileMappingW(FILE_MAP_READ, false, L"sRandomPhotoMemMap");
+	if (openMap == NULL) // It has not been saved yet, this is the first program instance
+	{
+		openMap = CreateFileMappingW
+		(
+			INVALID_HANDLE_VALUE,
+			NULL,
+			PAGE_READWRITE,
+			0,
+			120,
+			L"sRandomPhotoMemMap"
+		);
+		
+		if (openMap == NULL) MessageBox(NULL, L"It failed first?", L"", MB_OK);
+
+		LPVOID mapPtr = MapViewOfFile(openMap, FILE_MAP_ALL_ACCESS, 0, 0, 120);
+		if (mapPtr == NULL)
+		{
+			//MessageBox(NULL, L"It worked?", L"", MB_OK);
+			CopyMemory(mapPtr, &mutexName, 120);
+		}
+	}
+	else // The PID has been saved already, this is a second instance of the program
+	{
+		openMap = OpenFileMappingW(FILE_MAP_READ, false, L"sRandomPhotoMemMap");
+		if (openMap != NULL) 
+		{
+			uniqueID = MapViewOfFile(openMap, FILE_MAP_READ, 0, 0, 120);
+			if (uniqueID != nullptr)
+			{
+				//MessageBox(NULL, (wchar_t*)uniqueID, L"", MB_OK);
+
+				if (openImage == L"") // The user opened a new instance but is not opening an image with it
+					ExitProcess(0);
+				else // The user is trying to open an image directly with a new instance, close the original instance
+				{
+
+				}
+			}
+		}
+	}
+
 	SciterSetOption
 	(
 		NULL,
@@ -91,12 +147,13 @@ int uimain(std::function<int()> run)
 		ALLOW_SYSINFO |
 		ALLOW_EVAL
 	);
-	
+
 	sciter::archive::instance().open(aux::elements_of(resources)); 
 	
 	aux::asset_ptr<MainWindow> mainWin = new MainWindow;
 	mainWin->load(L"this://app/index.html");
 	mainWin->expand();	
+
 	::root = mainWin->get_root();
 
 	EventHandler eh;
